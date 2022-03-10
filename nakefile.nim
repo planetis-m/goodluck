739a7585cb24ee3353d7a567e5eb73197076c817
Antonis Geralis <b3lievem@yandex.com>
2022-03-10 23:00:57 +0200

    ofcourse the previous didn't work...

--- nakefile.nim
+++ nakefile.nim
@@ -8,7 +8,7 @@ task "docs", "Generate documentation":
     src = "rstdocs" / name
     doc = dir / name.changeFileExt(".html")
   if doc.needsRefresh(src):
-    direSilentShell("Generating the docs...", nimExe,
-        &"rst2html --verbosity:0 --out:{dir} {src}")
+    direSilentShell("Generating the docs...",
+        &"nim rst2html --out:{dir} {src}")
   else:
     echo "Skipped generating the docs."
